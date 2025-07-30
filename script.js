7935c44215471fc0bb320b67a0ed77957ef72a06
frixc7 <frixc7@gmail.com>
2025-07-30 14:18:33 +0100

    Create new book form with JS interactivity
    The form uses dialog method and has no action since it's all in the
    client-side.

--- script.js
+++ script.js
@@ -31,3 +31,37 @@ function displayBooks(library) {
 
 const myLibrary = [];
 const container = document.querySelector(".container");
+const new_book_open = document.querySelector("#new-book-button");
+const new_book_dialog = document.querySelector("#new-book-dialog");
+const new_book_form = document.querySelector("#new-book-dialog form");
+const new_book_close = document.querySelector("#new-book-dialog > button");
+const new_book_submit = document.querySelector('#new-book-dialog button[type="submit"]');
+
+new_book_open.addEventListener("click", () => {
+    new_book_dialog.showModal();
+});
+
+/* Another method to access form data (if method="dialog" not set)
+new_book_form.onsubmit = function(e) {
+    e.preventDefault();
+    console.log(new_book_form.title.value);
+    new_book_dialog.close();
+}
+*/
+
+/* Another method to access form data (if method="dialog" not set) | the form DOM selection needs to be declared here instead
+new_book_submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const new_book_form = new FormData(document.querySelector("#new-book-dialog form"));
+    console.log(new_book_form.get("title"));
+    new_book_dialog.close();
+});
+*/
+
+new_book_submit.addEventListener("click", () => {
+    addBook(new_book_form.title.value, new_book_form.author.value, new_book_form.pages.value, new_book_form.read.value);
+});
+
+new_book_close.addEventListener("click", () => {
+    new_book_dialog.close();
+});
