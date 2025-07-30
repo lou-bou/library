e441f25343e30d166df7838c8b3ad2cb7846c255
frixc7 <frixc7@gmail.com>
2025-07-30 11:48:34 +0100

    Create function to display all books in divs
    Each div acts as a card for one book.
    All divs are in the .container div.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 
     this.info = function () {
-        return this.title + " by " + this.author + ", " + this.pages + " pages, " (this.read? "already read." : "not read yet.");
+        return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.read? "already read." : "not read yet.");
     }
 }
 
@@ -19,5 +19,15 @@ function addBook(title, author, pages, read) {
     myLibrary.push(book);
 }
 
-const myLibrary = [];
+function displayBooks(library) {
+    let book_card;
+    for (const book of library) {
+        book_card = document.createElement("div");
+        book_card.classList.add = "book-card";
+        book_card.textContent = book.info();
+        container.appendChild(book_card);
+    }
+}
 
+const myLibrary = [];
+const container = document.querySelector(".container");
