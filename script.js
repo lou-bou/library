03f8327e362f14b75ac7fa0e592937045a7ece18
frixc7 <frixc7@gmail.com>
2025-07-30 11:27:30 +0100

    Create library array, addBook function and book ID
    - Create an array to store books in
    - Create a function to create a new book
    - Give each new book a unique ID (defined in constructor)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function Book(title, author, pages, read) {
         throw Error("You must use the 'new' keyword to call the constructor.");
     }
 
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -12,3 +13,11 @@ function Book(title, author, pages, read) {
         return this.title + " by " + this.author + ", " + this.pages + " pages, " (this.read? "already read." : "not read yet.");
     }
 }
+
+function addBook(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+const myLibrary = [];
+
